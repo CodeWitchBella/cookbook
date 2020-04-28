e8d234c40b05def9c8ded140aca20cc1421e9799
Isabella Skořepová <isabella@skorepova.info>
2020-04-28 22:44:48 +0200

    put object finished

--- api/put-object.ts
+++ api/put-object.ts
@@ -1,11 +1,27 @@
-import { promiseRequest, checkCSRF } from './_promise-request'
+import { promiseRequest, checkCSRF, loggedInUserId } from './_promise-request'
+import { firestore } from './_firestore'
 
 export default promiseRequest(async (req, res) => {
   checkCSRF(req)
   const { id, type, data } = req.body
-  if (!id || !type || !data) {
+  if (!type || !data) {
     res.status(400)
     return 'Missing body field'
   }
-  return 'todo'
+  const userId = await loggedInUserId(req)
+  if (!userId) {
+    res.status(403)
+    return 'Not logged in'
+  }
+
+  if (id) {
+    await firestore
+      .collection('object')
+      .doc(id)
+      .set({ ...data, type }, { merge: true })
+    return id
+  } else {
+    const doc = await firestore.collection('object').add({ ...data, type })
+    return doc.id
+  }
 })
