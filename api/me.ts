e51513959d891b11afeb0a05fa0f2d912e710c21
Isabella Skořepová <isabella@skorepova.info>
2020-04-30 20:51:24 +0200

    extra data for user

--- api/me.ts
+++ api/me.ts
@@ -5,9 +5,9 @@ export default promiseRequest(async (req, res) => {
   checkCSRF(req, 'GET')
   const userId = await loggedInUserId(req)
   if (!userId) {
-    return { error: 'Not logged in' }
+    return { error: 'Not logged in', code: 'NOT_LOGGED_IN' }
   }
   const user = await firestore.collection('user').doc(userId).get()
   if (!user.exists) return { error: 'Not logged in' }
-  return { ...user.data(), passwordHash: undefined }
+  return { id: user.id, ...user.data(), passwordHash: undefined }
 })
